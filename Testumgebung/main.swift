7744978174109f97f1c6cd989f0f53be6b9eb315
Geordie Jay <geordiejay@me.com>
2016-01-09 03:08:16 +0100

    Initial Commit

---
+++ Testumgebung/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Testumgebung
+//
+//  Created by Geordie Jay on 09.01.16.
+//  Copyright © 2016 Geordie Jay. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
